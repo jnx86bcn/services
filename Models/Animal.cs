c32d397817ea3f3592ebbf9f3598ec5ea4a1e034
Juan Gasparino <juan.gasparino@birchmangroup.com>
2018-10-24 20:02:28 +0200

    first commit

---
+++ Models/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Models
+{
+    [DataContract]
+    public class Animal
+    {
+        [DataMember][BsonId] public ObjectId Id { get; set; }
+        [DataMember] [BsonElement("MDB_Name")] public string Name { get; set; }
+        [DataMember] [BsonElement("MDB_Kingdom")] public string Kingdom { get; set; }
+        [DataMember] [BsonElement("MDB_Class")] public string Class { get; set; }
+        [DataMember] [BsonElement("MDB_ConservationStatus")] public string ConservationStatus { get; set; }
+        [DataMember] [BsonElement("MDB_Region")] public string Region { get; set; }
+        [DataMember] [BsonElement("MDB_Extinct")] public bool Extinct { get; set; }
+        [DataMember] [BsonElement("MDB_Birth")] public DateTime Birth { get; set; }
+        [DataMember] [BsonElement("MDB_Death")] public DateTime Death { get; set; }
+    }
+}
---
+++ zoo/App_Code/Service.cs
@@ -0,0 +1,85 @@
+﻿using System.Configuration;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using MongoDB.Driver;
+using Models;
+using System;
+using System.Collections.Generic;
+
+namespace zoo
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class Service : IService
+    {
+        string _connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
+
+        public List<Animal> GetAllItems()
+        {
+
+            //Create client connection to our MongoDB database
+            var client = new MongoClient(_connectionString);
+
+            //Create a session object that is used when leveraging transactions
+            var session = client.StartSession();
+
+            //Create the collection object that represents the "products" collection
+            IMongoCollection<Animal> animalsCollection = session.Client.GetDatabase("MongoDBStore").GetCollection<Animal>("animals");
+
+            //Begin transaction
+            session.StartTransaction();
+
+            try
+            {
+                var filter = new FilterDefinitionBuilder<Animal>().Empty;
+                List<Animal> results = animalsCollection.Find<Animal>(filter).ToList();
+
+                //Made it here without error? Let's commit the transaction
+                session.CommitTransaction();
+
+                return results;
+            }
+            catch (Exception)
+            {
+                session.AbortTransaction();
+                return null;
+            }
+
+        }
+
+        public void AddItem(Animal animal)
+        {
+
+            //Create client connection to our MongoDB database
+            var client = new MongoClient(_connectionString);
+
+            //Create a session object that is used when leveraging transactions
+            var session = client.StartSession();
+
+            //Create the collection object that represents the "products" collection
+            var animals = session.Client.GetDatabase("MongoDBStore").GetCollection<Animal>("animals");
+
+            //Begin transaction
+            session.StartTransaction();
+
+            try
+            {
+                //Insert the sample data 
+                animals.InsertOneAsync(animal);
+
+                //var filter = new FilterDefinitionBuilder<Animal>().Empty;
+                //var results = animals.Find<Animal>(filter).ToListAsync();
+
+                //Made it here without error? Let's commit the transaction
+                session.CommitTransaction();
+            }
+            catch (Exception)
+            {
+                session.AbortTransaction();
+
+            }
+
+        }
+
+    }
+}

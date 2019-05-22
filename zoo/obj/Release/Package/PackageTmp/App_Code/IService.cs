690475aba6d7130bf895766e76273ceb709e1977
jnx86bcn <jnx86bcn@gmail.com>
2019-05-22 22:14:42 +0200

    commit

---
+++ zoo/obj/Release/Package/PackageTmp/App_Code/IService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using Models;
+
+namespace zoo
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/GetAllItems", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        List<Animal> GetAllItems();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/AddItem", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        void AddItem(string json);
+    }
+}
---
+++ zoo/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="zoo.Global" Language="C#" %>
